18baf8553cf09a3b8d8bbe4658141a79011ecc17
Ravi Kumar V <ravikumar.v@ltimindtree.com>
2025-04-21 18:04:13 +0530

    Add Fibonacci and GCD functions with main execution block

---
+++ C_1/gcd.py
@@ -0,0 +1,22 @@
+def naive_gcd(a, b):
+  result= min(a,b)
+  while result > 0:
+    if a % result == 0 and b % result == 0:
+      return result
+    result -= 1
+  return 1
+
+def gcd_euclidean(a, b):
+  if b==0:
+    return a
+  if a==0:
+    return b
+  if a > b:
+    return gcd_euclidean(a % b, b)
+  else:
+    return gcd_euclidean(a, b % a)
+
+if __name__ == "__main__":
+  a,b= map(int, input().split())
+  print(naive_gcd(a,b))
+  print(gcd_euclidean(a,b))
