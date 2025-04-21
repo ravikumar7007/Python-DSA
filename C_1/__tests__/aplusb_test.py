b7a0c33529872186ea55ab48c9afedc3df5d0746
Ravi Kumar V <ravikumar.v@ltimindtree.com>
2025-04-21 15:55:21 +0530

    Refactor test cases for sumOfTwoNumbers function and update unittestArgs for test directory

--- C_1/__tests__/aplusb_test.py
+++ C_1/__tests__/aplusb_test.py
@@ -1,24 +1,18 @@
 import unittest
 from C_1.aplusb import sumOfTwoNumbers
 
-
 class TestSumOfTwoNumbers(unittest.TestCase):
     def test_positive_numbers(self):
-        self.assertEqual(sumOfTwoNumbers(3, 5), 8)
+        self.assertEqual(sumOfTwoNumbers(2, 3), 5)
 
     def test_negative_numbers(self):
-        self.assertEqual(sumOfTwoNumbers(-3, -5), -8)
+        self.assertEqual(sumOfTwoNumbers(-2, -3), -5)
 
     def test_mixed_sign_numbers(self):
-        self.assertEqual(sumOfTwoNumbers(-3, 5), 2)
+        self.assertEqual(sumOfTwoNumbers(-2, 3), 1)
 
-    def test_zero_and_number(self):
-        self.assertEqual(sumOfTwoNumbers(0, 5), 5)
-        self.assertEqual(sumOfTwoNumbers(5, 0), 5)
-
-    def test_large_numbers(self):
-        self.assertEqual(sumOfTwoNumbers(1000000, 2000000), 3000000)
-
+    def test_zero(self):
+        self.assertEqual(sumOfTwoNumbers(0, 0), 0)
 
 if __name__ == "__main__":
     unittest.main()
