18f61ba09c46f348c5c188d1aef3ff6fe71af477
Ravi Kumar V <ravikumar.v@ltimindtree.com>
2025-04-21 10:59:37 +0530

    Add function to sum two integers with example usage

---
+++ C-1/aplusb.py
@@ -0,0 +1,16 @@
+def sumOfTwoNumbers(a: int, b: int) -> int:
+    """
+    This function takes two integers a and b, and returns their sum.
+    
+    :param a: First integer
+    :param b: Second integer
+    :return: Sum of a and b
+    """
+    return a + b
+
+if __name__ == "__main__":
+    # Example usage
+    arr= input().split()
+    a = int(arr[0])
+    b = int(arr[1])
+    print(sumOfTwoNumbers(a, b))
