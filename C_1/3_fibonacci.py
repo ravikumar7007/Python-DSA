8fd0269cb134695f572e0009022c92b40b893bb0
Ravi Kumar V <ravikumar.v@ltimindtree.com>
2025-04-21 21:35:27 +0530

    Refactor functions for consistent formatting and improved readability in maximum pairwise product, Fibonacci, and GCD implementations.

--- C_1/3_fibonacci.py
+++ C_1/3_fibonacci.py
@@ -1,22 +1,23 @@
-def fibonacci(n: int)-> int:
-  if n==1:
-    return 0
-  if n==2:
-    return 1 
-  return fibonacci(n-1) + fibonacci(n-2)
+def fibonacci(n: int) -> int:
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
-def fibonacci_fast(n: int)-> int:
-  if n==0:
-    return 0
-  if n==1:
-    return 1
-  fib: list[int] = [0,1]
-  for i in range(2,n+1):
-    fib.append(fib[i-1]+fib[i-2])
-  
-  return fib[n]
+
+def fibonacci_fast(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    fib: list[int] = [0, 1]
+    for i in range(2, n+1):
+        fib.append(fib[i-1]+fib[i-2])
+
+    return fib[n]
 
 
 if __name__ == "__main__":
-  n = int(input("Enter a number: "))
-  print(f"The {n}th Fibonacci number is: {fibonacci_fast(n)}")
+    n = int(input("Enter a number: "))
+    print(f"The {n}th Fibonacci number is: {fibonacci_fast(n)}")
--- C_1/4_gcd.py
+++ C_1/4_gcd.py
@@ -1,22 +1,24 @@
 def naive_gcd(a, b):
-  result= min(a,b)
-  while result > 0:
-    if a % result == 0 and b % result == 0:
-      return result
-    result -= 1
-  return 1
+    result = min(a, b)
+    while result > 0:
+        if a % result == 0 and b % result == 0:
+            return result
+        result -= 1
+    return 1
+
 
 def gcd_euclidean(a, b):
-  if b==0:
-    return a
-  if a==0:
-    return b
-  if a > b:
-    return gcd_euclidean(a % b, b)
-  else:
-    return gcd_euclidean(a, b % a)
+    if b == 0:
+        return a
+    if a == 0:
+        return b
+    if a > b:
+        return gcd_euclidean(a % b, b)
+    else:
+        return gcd_euclidean(a, b % a)
+
 
 if __name__ == "__main__":
-  a,b= map(int, input().split())
-  print(naive_gcd(a,b))
-  print(gcd_euclidean(a,b))
+    a, b = map(int, input().split())
+    print(naive_gcd(a, b))
+    print(gcd_euclidean(a, b))
