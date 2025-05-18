8f4bc179c06ad1930f3289a9f05c3d85a2389cdd
Ravi Kumar V <71925220+ravikumar7007@users.noreply.github.com>
2025-05-18 23:12:56 +0530

    Refactor colorTheGrid and countWays methods for improved readability and consistency

--- test.py
+++ test.py
@@ -1,37 +1,39 @@
-class Solution(object):
-    mod = 10**9 + 7
+class Solution:
+    MOD = 10**9 + 7
 
     def __init__(self):
-        self.state_mem = [[-1] * 1024 for _ in range(1002)]
+        self.state_mem = [
+            [-1] * 1024 for _ in range(1002)
+        ]  # 1000 rows + 2, 1024 for 10 bits
 
-    def colorTheGrid(self, m, n):
-        return self.countways(m, n, 0, 0, 0, 0)
+    def colorTheGrid(self, m: int, n: int) -> int:
+        return self.countWays(m, n, 0, 0, 0, 0)
 
-    def countways(self, m, n, r, c, curr, prev):
+    def countWays(self, m, n, r, c, curr_state, prev_state):
         if c == n:
             return 1
         if r == m:
-            return self.countways(m, n, 0, c + 1, 0, curr)
-        if r == 0 and self.state_mem[c][prev] != -1:
-            return self.state_mem[c][prev]
+            return self.countWays(m, n, 0, c + 1, 0, curr_state)
+        if r == 0 and self.state_mem[c][prev_state] != -1:
+            return self.state_mem[c][prev_state]
 
-        up_color = curr & 3 if r > 0 else 0
+        up_color = 0
+        if r > 0:
+            up_color = curr_state & 3
 
-        shift = 2 * (m - r - 1)
-
-        left_color = (prev >> shift) & 3
+        left_color = (prev_state >> ((m - r - 1) * 2)) & 3
 
         ways_to_color = 0
-
         for color in range(1, 4):
             if color != up_color and color != left_color:
-                new_state = curr << 2 | color
+                new_state = (curr_state << 2) | color
                 ways_to_color = (
-                    ways_to_color + self.countways(m, n, r + 1, c, new_state, prev)
-                ) % self.mod
+                    ways_to_color
+                    + self.countWays(m, n, r + 1, c, new_state, prev_state)
+                ) % self.MOD
 
         if r == 0:
-            self.state_mem[c][prev] = ways_to_color
+            self.state_mem[c][prev_state] = ways_to_color
         return ways_to_color
 
 
