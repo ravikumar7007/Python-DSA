44bdfaf703e6d32d1178566bf2308edb5fd92bbc
Ravi Kumar V <71925220+ravikumar7007@users.noreply.github.com>
2025-05-21 21:50:16 +0530

    Refactor setZeroes method to modify matrix in-place and improve readability

--- test.py
+++ test.py
@@ -1,27 +1,39 @@
+
 from typing import List
 
-
 class Solution:
-    def triangleType(self, nums: List[int]) -> str:
-        a, b, c = sorted(nums)
-        if a + b <= c:
-            return "none"
-        s = set(nums)
-        if len(s) == 1:
-            return "equilateral"
-        elif len(s) == 2:
-            return "isosceles"
-        else:
-            return "scalene"
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix [0][j] = 0
+                    matrix [i][0] = 0
 
+        for i in range(1,m):
+            if matrix[i][0] == 0:
+                for j in range(1,n):
+                    matrix[i][j] = 0
+        
+        for j in range(1,n):
+            if matrix[0][j] == 0:
+                for i in range(1,m):
+                    matrix[i][j] = 0
+        
+        if matrix[0][0] == 0:
+            for i in range(m):
+                matrix[i][0] = 0
+            for j in range(n):
+                matrix[0][j] = 0
+         
+        
 
 if __name__ == "__main__":
+    matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
     s = Solution()
-    nums = [3, 4, 5]
-    print(s.triangleType(nums))  # Output: scalene
-    nums = [2, 2, 2]
-    print(s.triangleType(nums))  # Output: equilateral
-    nums = [2, 2, 3]
-    print(s.triangleType(nums))  # Output: isosceles
-    nums = [1, 2, 3]
-    print(s.triangleType(nums))  # Output: none
+    s.setZeroes(matrix)
+    print(matrix)
