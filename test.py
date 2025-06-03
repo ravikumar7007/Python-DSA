86bcefbc31fb3db219ce09ded233f62ff5c3474b
Ravi Kumar V <71925220+ravikumar7007@users.noreply.github.com>
2025-06-03 20:14:32 +0530

    Refactor and implement maxCandies method to calculate the total number of candies based on box status and keys

--- test.py
+++ test.py
@@ -1,33 +1,47 @@
+from collections import deque
 from typing import List
 
 
 class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        if n == 0:
-            return 0
-        res = [1] * n
-        # Forward pass
-        for i in range(1, n):
-            if ratings[i] > ratings[i - 1]:
-                res[i] = res[i - 1] + 1
-        # Backward pass
-        total = res[-1]
-        for i in range(n - 2, -1, -1):
-            if ratings[i] > ratings[i + 1] and res[i] <= res[i + 1]:
-                res[i] = res[i + 1] + 1
-            total += res[i]
+    def maxCandies(
+        self,
+        status: List[int],
+        candies: List[int],
+        keys: List[List[int]],
+        containedBoxes: List[List[int]],
+        initialBoxes: List[int],
+    ) -> int:
+        n = len(status)
+        q = deque(initialBoxes)
+        closed = set()
+        total = 0
+
+        while q:
+            curr = q.popleft()
+
+            if status[curr] == 0:
+                closed.add(curr)
+                continue
+
+            for open in keys[curr]:
+                status[open] = 1
+                if open in closed:
+                    q.append(open)
+                    closed.remove(open)
+
+            total += candies[curr]
+            for box in containedBoxes[curr]:
+                q.append(box)
+
         return total
 
-
 if __name__ == "__main__":
-    ratings = [1, 2, 87, 87, 87, 2, 1]
     solution = Solution()
-    print(solution.candy(ratings))  # Output: 13
-    ratings = [1, 0, 2]
-    solution = Solution()
-    print(solution.candy(ratings))  # Output: 5
-    ratings = [1, 2, 2]
-    print(solution.candy(ratings))  # Output: 4
-    ratings = [1, 3, 2, 2, 1]
-    print(solution.candy(ratings))  # Output: 9
+    status = [1, 0, 1]
+    candies = [5, 2, 3]
+    keys = [[], [0], []]
+    containedBoxes = [[1], [], []]
+    initialBoxes = [0]
+
+    result = solution.maxCandies(status, candies, keys, containedBoxes, initialBoxes)
+    print(result)  # Expected output: 5
