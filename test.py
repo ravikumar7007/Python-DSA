8b8d22978536c42938bf5f12f6d11759166bfdc0
Ravi Kumar V <71925220+ravikumar7007@users.noreply.github.com>
2025-06-05 21:52:30 +0530

    Refactor and implement answerString method to find the lexicographically largest substring based on the number of friends

--- test.py
+++ test.py
@@ -1,47 +1,19 @@
-from collections import deque
-from typing import List
-
-
 class Solution:
-    def maxCandies(
-        self,
-        status: List[int],
-        candies: List[int],
-        keys: List[List[int]],
-        containedBoxes: List[List[int]],
-        initialBoxes: List[int],
-    ) -> int:
-        n = len(status)
-        q = deque(initialBoxes)
-        closed = set()
-        total = 0
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
+        n = len(word)
+        max_len = n - numFriends + 1
+        res = ""
+        for i in range(n):
+            wor = word[i : (i + max_len) or n]
+            if wor > res:
+                res = wor
+        return res
 
-        while q:
-            curr = q.popleft()
-
-            if status[curr] == 0:
-                closed.add(curr)
-                continue
-
-            for open in keys[curr]:
-                status[open] = 1
-                if open in closed:
-                    q.append(open)
-                    closed.remove(open)
-
-            total += candies[curr]
-            for box in containedBoxes[curr]:
-                q.append(box)
-
-        return total
 
 if __name__ == "__main__":
-    solution = Solution()
-    status = [1, 0, 1]
-    candies = [5, 2, 3]
-    keys = [[], [0], []]
-    containedBoxes = [[1], [], []]
-    initialBoxes = [0]
-
-    result = solution.maxCandies(status, candies, keys, containedBoxes, initialBoxes)
-    print(result)  # Expected output: 5
+    s = Solution()
+    ans = s.answerString("aann", 2)
+    print(ans)
+    # Output: # "dbca", "bcad", "cadb", "adbc"
