c18de2a5774042dacd0d8ea34d4206fd4b5c3388
Ravi Kumar V <71925220+ravikumar7007@users.noreply.github.com>
2025-06-06 21:28:07 +0530

    Refactor and implement robotWithString method to rearrange characters in a string based on lexicographical order

--- test.py
+++ test.py
@@ -1,19 +1,40 @@
+from typing import List
+
+
 class Solution:
-    def answerString(self, word: str, numFriends: int) -> str:
-        if numFriends == 1:
-            return word
-        n = len(word)
-        max_len = n - numFriends + 1
-        res = ""
-        for i in range(n):
-            wor = word[i : (i + max_len) or n]
-            if wor > res:
-                res = wor
-        return res
+    def smallest_char(self, dic: List[int]) -> str:
+        for i in range(26):
+            if dic[i] > 0:
+                return chr(i + 97)
+        return "z"
+
+    def robotWithString(self, s: str) -> str:
+        n = len(s)
+        dic = [0] * 26
+        for c in s:
+            dic[ord(c) - 97] += 1
+
+        t = []
+        res = []
+        pos = 0
+
+        while pos < n:
+            t.append(s[pos])
+            dic[ord(s[pos]) - 97] -= 1
+            smallest = ord(self.smallest_char(dic))
+            while t and ord(t[-1]) <= smallest:
+                res.append(t.pop())
+            pos += 1
+
+        return "".join(res)
 
 
 if __name__ == "__main__":
-    s = Solution()
-    ans = s.answerString("aann", 2)
-    print(ans)
-    # Output: # "dbca", "bcad", "cadb", "adbc"
+    s = "cba"
+    print(Solution().robotWithString(s))  # Output: "abc"
+
+    s = "leetcode"
+    print(Solution().robotWithString(s))  # Output: "cdelotee"
+
+    s = "aaaabbbbcccc"
+    print(Solution().robotWithString(s))  # Output: "aaaabbbbcccc"
